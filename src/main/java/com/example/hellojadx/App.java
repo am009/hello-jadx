25d8f310d73cff6155c559aa9edd0ab72879411a
am009 <warrenwjk@qq.com>
2024-07-05 06:05:41 +0000

    Initial project.

--- src/main/java/com/example/hellojadx/App.java
+++ src/main/java/com/example/hellojadx/App.java
@@ -1,13 +1,48 @@
 package com.example.hellojadx;
 
+import java.io.File;
+
+import jadx.api.JadxArgs;
+import jadx.api.JadxDecompiler;
+import jadx.api.JavaClass;
+import jadx.api.JavaMethod;
+import jadx.core.dex.instructions.InvokeNode;
+import jadx.core.dex.nodes.BlockNode;
+import jadx.core.dex.nodes.InsnNode;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String path = args[0];
+        // jadx api 解析APK
+        JadxArgs jadxArgs = new JadxArgs();
+        jadxArgs.setInputFile(new File(path));
+        jadxArgs.setOutDir(new File("output"));
+        try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
+            jadx.load();
+            jadx.save();
+            for (JavaClass cls : jadx.getClasses()) {
+                for (JavaMethod mth : cls.getMethods()) {
+                    if (mth.getMethodNode().getBasicBlocks() == null) { continue; }
+                    for (BlockNode block : mth.getMethodNode().getBasicBlocks()) {
+                        for( InsnNode a: block.getInstructions()) {
+                            if (a instanceof InvokeNode) {
+                                InvokeNode invokeNode = (InvokeNode) a;
+                                String name = invokeNode.getCallMth().getName();
+                                String clazz = invokeNode.getCallMth().getDeclClass().getFullName();
+                                System.out.println("调用方法：" + clazz + "." + name);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
