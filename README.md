6f3f5fd6dd7c5cfe2bcd9c09196c0c47c7e71a0b
am009 <warrenwjk@qq.com>
2024-07-05 06:33:46 +0000

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello-Jadx
+
+Use Jadx as a libaray: https://github.com/skylot/jadx/wiki/Use-jadx-as-a-library
+
+打包为单个jar文件： mvn clean compile assembly:single
+
+然后会生成jar文件： ./target/hello-jadx-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/com/example/hellojadx/App.java
+++ src/main/java/com/example/hellojadx/App.java
@@ -6,6 +6,7 @@ import jadx.api.JadxArgs;
 import jadx.api.JadxDecompiler;
 import jadx.api.JavaClass;
 import jadx.api.JavaMethod;
+import jadx.api.impl.SimpleCodeWriter;
 import jadx.core.dex.instructions.InvokeNode;
 import jadx.core.dex.nodes.BlockNode;
 import jadx.core.dex.nodes.InsnNode;
@@ -17,10 +18,12 @@ import jadx.core.dex.nodes.InsnNode;
 public class App {
     public static void main(String[] args) {
         String path = args[0];
+        // String to_find = args[1];
         // jadx api 解析APK
         JadxArgs jadxArgs = new JadxArgs();
         jadxArgs.setInputFile(new File(path));
         jadxArgs.setOutDir(new File("output"));
+        jadxArgs.setCodeWriterProvider(SimpleCodeWriter::new);
         try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
             jadx.load();
             jadx.save();
@@ -28,12 +31,15 @@ public class App {
                 for (JavaMethod mth : cls.getMethods()) {
                     if (mth.getMethodNode().getBasicBlocks() == null) { continue; }
                     for (BlockNode block : mth.getMethodNode().getBasicBlocks()) {
-                        for( InsnNode a: block.getInstructions()) {
+                        for(InsnNode a: block.getInstructions()) {
                             if (a instanceof InvokeNode) {
                                 InvokeNode invokeNode = (InvokeNode) a;
                                 String name = invokeNode.getCallMth().getName();
                                 String clazz = invokeNode.getCallMth().getDeclClass().getFullName();
                                 System.out.println("调用方法：" + clazz + "." + name);
+                                // if ((clazz+'.'+name).equals(to_find)) {
+                                //     System.out.println(mth.getFullName() + "调用了想找的方法");
+                                // }
                             }
                         }
                     }
@@ -42,7 +48,5 @@ public class App {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
